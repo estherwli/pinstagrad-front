a482fa934864625c7dfff444819690926eafee8c
Esther Li <li.esther2000@gmail.com>
2019-11-03 12:44:10 -0800

    added more pages

---
+++ src/pages/registerPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      
+          Register page
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/uploadPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      
+          Upload page
+
+    </div>
+  );
+}
+
+export default App;
