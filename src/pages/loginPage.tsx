17bf3cd4a1ff8d7bd259b4875504fa05259d2c4d
marcuspearce <marcuspearce7@gmail.com>
2019-11-03 14:37:06 -0800

    proto loginPage

--- src/pages/loginPage.tsx
+++ src/pages/loginPage.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import "tabler-react/dist/Tabler.css"
+import {Form, Card, Button } from "tabler-react"
+
+import "./loginPage.css"
+
+import pinstagradLogo from "../assets/PinstagradLogo.jpeg"
+
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      
-          Login
+
+    <div className="loginPage">
+
+        {/*  link correct font */ }
+        <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet"></link>
+
+
+        <img src={pinstagradLogo} alt="Pinstagrad Logo" />
+
+        <div className="loginForm">
+          <Form onSubmit={(event) => console.log(event.target.name + 'clicked')}>
+            <Form.Input className="loginTextBox" name='username' label='Username' placeholder='Enter Username' />
+            <Form.Input className="loginTextBox" name='password' label='Password' type="password" placeholder='Enter Password' />
+            <Button type='submit' value='Submit' />
+          </Form>
+        </div>
 
     </div>
   );
