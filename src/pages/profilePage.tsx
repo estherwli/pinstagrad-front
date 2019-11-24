84b383734dd4183ca12df73bbb1f4461ec0a29a8
Chelsey Wang <chelseyyywang@gmail.com>
2019-11-24 14:33:29 -0800

    commit msg

--- src/pages/profilePage.tsx
+++ src/pages/profilePage.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import "tabler-react/dist/Tabler.css";
+import "./profilePage.css"
+import { Avatar, Button } from "tabler-react";
+ 
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <header>
+        <link href="https://fonts.googleapis.com/css?family=Montserrat|Nanum+Gothic|Open+Sans&display=swap" rel="stylesheet"></link>
+        <link href="https://fonts.googleapis.com/css?family=Nanum+Gothic|Open+Sans&display=swap" rel="stylesheet"></link>
+        <link href="https://fonts.googleapis.com/css?family=Nanum+Gothic&display=swap" rel="stylesheet"></link>
+      </header>
+        <img src="https://github.com/chelseyywang/pinstagrad/blob/master/pinstagrad.png?raw=true" className="logo" alt='logo'/>
+        <h1 className="title">PROFILE</h1>
+        <div className="pppic"><Avatar size="xxl" imageURL="https://media.gq.com/photos/5a74702054cb6a6e81fe34df/master/w_775%2Cc_limit/rich-brian-web-alex-reside13.jpg" /></div>
+       
+        
       
-          Profile page
-
+      <div><Button pill color="secondary">
+              Upload Photo
+      </Button></div>
+      {/* <Button pill color="secondary">
+              Your Feed
+      </Button> */}
+      <div className="ppquestion"><strong>Your Feed: </strong></div>
+      {/* <button className="butt"> 
+        <img src="https://github.com/chelseyywang/DZknow/blob/master/dzcursor.gif?raw=true"></img> 
+      </button> */}
+      {/* <input type="image" src="https://github.com/chelseyywang/DZknow/blob/master/dzcursor.gif?raw=true" /> */}
+      <input type="image" src="https://github.com/chelseyywang/pinstagrad/blob/master/arrow%201.png?raw=true" />
     </div>
   );
 }
