2fed721e972348dde3807fcfe92dc0b69355105c
marcuspearce <marcuspearce7@gmail.com>
2019-11-24 16:38:49 -0800

    updated profile page

--- src/pages/profilePage.tsx
+++ src/pages/profilePage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "tabler-react/dist/Tabler.css";
 import "./profilePage.css"
-import { Avatar, Button } from "tabler-react";
+import { Avatar, Button, Dropdown } from "tabler-react";
  
 
 const App: React.FC = () => {
@@ -12,26 +12,88 @@ const App: React.FC = () => {
         <link href="https://fonts.googleapis.com/css?family=Nanum+Gothic|Open+Sans&display=swap" rel="stylesheet"></link>
         <link href="https://fonts.googleapis.com/css?family=Nanum+Gothic&display=swap" rel="stylesheet"></link>
       </header>
-        <img src="https://github.com/chelseyywang/pinstagrad/blob/master/pinstagrad.png?raw=true" className="logo" alt='logo'/>
-        <h1 className="title">PROFILE</h1>
-        <div className="pppic"><Avatar size="xxl" imageURL="https://media.gq.com/photos/5a74702054cb6a6e81fe34df/master/w_775%2Cc_limit/rich-brian-web-alex-reside13.jpg" /></div>
-       
+
+
+
+      <div className="container">
+
+        <div>
+          <img src="https://github.com/chelseyywang/pinstagrad/blob/master/pinstagrad.png?raw=true" className="logo" alt='logo'/>
+        </div>
+
+        <div className="title">
+          <h1 className="titleText">PROFILE</h1>
+        </div>
+
+        <div className='heart'>
+          <Button icon='heart' color="azure"></Button>
+        </div>
+
+        <div className='dropdown'>
+          <Button.List>
+            <Dropdown
+              type="button"
+              toggle
+              color="secondary"
+              items={[
+                <Dropdown.Item>Home</Dropdown.Item>,
+                <Dropdown.Item>Mission</Dropdown.Item>,
+                <Dropdown.Item>Profile</Dropdown.Item>,
+                <Dropdown.Item>Log Out</Dropdown.Item>
+              ]}
+            />
+            <Dropdown
+              value="Show Calendar"
+              color="secondary"
+              icon="calendar"
+              items={[
+                <Dropdown.Item>Dropdown Link</Dropdown.Item>,
+                <Dropdown.Item>Dropdown Link</Dropdown.Item>,
+              ]}
+            />
+            <Dropdown
+              value="Show Calendar"
+              color="primary"
+              toggle
+              
+              items={[
+                <Dropdown.Item>Dropdown Link</Dropdown.Item>,
+                <Dropdown.Item>Dropdown Link</Dropdown.Item>,
+              ]}
+            />
+          </Button.List>
+        </div>
+
+      </div>
+
+
+
+
+
+
+      <div className="centredContent">
+
+         <img src="https://media.gq.com/photos/5a74702054cb6a6e81fe34df/master/w_775%2Cc_limit/rich-brian-web-alex-reside13.jpg" className="profilePic" alt='profilePic'/>
+
+        <h1><em>Username Here</em></h1>
+
+        <div className="buttonDiv">
+          <button color="yellow" className="uploadButton">Upload a Picture!</button>
+        </div>
+
+
+        <div className="linkDiv">
+          <h2>To Your Uploaded Pictures</h2>>
+          <input type="image" src="https://github.com/chelseyywang/pinstagrad/blob/master/arrow%201.png?raw=true" />
+        </div>
         
-      
-      <div><Button pill color="secondary">
-              Upload Photo
-      </Button></div>
-      {/* <Button pill color="secondary">
-              Your Feed
-      </Button> */}
-      <div className="ppquestion"><strong>Your Feed: </strong></div>
-      {/* <button className="butt"> 
-        <img src="https://github.com/chelseyywang/DZknow/blob/master/dzcursor.gif?raw=true"></img> 
-      </button> */}
-      {/* <input type="image" src="https://github.com/chelseyywang/DZknow/blob/master/dzcursor.gif?raw=true" /> */}
-      <input type="image" src="https://github.com/chelseyywang/pinstagrad/blob/master/arrow%201.png?raw=true" />
+
+      </div>
+
     </div>
   );
+
+  
 }
 
 export default App;
